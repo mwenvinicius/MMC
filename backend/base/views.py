91f3f76126b8162aa9b1ba5fa0e96fd639b1be49
mwenvinicius <viniciusdias@ufpi.edu.br>
2024-01-08 14:14:04 -0300

    #06 Correções.

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
